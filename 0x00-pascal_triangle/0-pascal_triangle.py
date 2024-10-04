242335d8a4350960774e1e914b257fbe6e96d7d5
Saviour Assandoh <asandohs@gmail.com>
2024-10-04 03:25:41 +0000

    Pascals Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,8 +7,9 @@ def pascal_triangle(n):
     if(n <= 0):
         return []
     else:
-        pascal_val = [[1] * (row + 1) for row in range(n)]  # Create initial structure of 1's
+        pascal_val = [[1] * (row + 1) for row in range(n)]
         for row in range(2, n):
             for col in range(1, row):
-                pascal_val[row][col] = pascal_val[row - 1][col - 1] + pascal_val[row - 1][col]
+                pascal_val[row][col] = (pascal_val[row - 1][col - 1] +
+                                        pascal_val[row - 1][col])
         return pascal_val
